64345fcc877aa52cbbb70954c89fbaa86539651a
rizky <rizky.fpa@gmail.com>
2026-02-10 09:13:12 +0700

    -

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -1,3 +1,14 @@
+export async function onRequestOptions() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, X-SECRET"
+    }
+  })
+}
+
 export async function onRequestPost({ request, env }) {
   const res = await fetch(env.BACKEND_URL + "/api/chat", {
     method: "POST",
@@ -9,6 +20,10 @@ export async function onRequestPost({ request, env }) {
   })
 
   return new Response(await res.text(), {
-    headers: { "Content-Type": "application/json" }
+    status: res.status,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
   })
 }
